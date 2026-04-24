27287c2c384fca537553c392ebc799d4fab41c33
anjuhs <anjanahs85@gmail.com>
2026-04-24 21:09:56 +0530

    Added update and delete APIs (full CRUD)

--- main.py
+++ main.py
@@ -25,3 +25,33 @@ def create_task(title: str, priority: str, db: Session = Depends(get_db)):
 @app.get("/tasks")
 def get_tasks(db: Session = Depends(get_db)):
     return db.query(models.Task).all()
+
+from fastapi import HTTPException
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, title: str, priority: str, completed: bool, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    task.title = title
+    task.priority = priority
+    task.completed = completed
+    
+    db.commit()
+    db.refresh(task)
+    return task
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    db.delete(task)
+    db.commit()
+    
+    return {"message": "Task deleted successfully"}
